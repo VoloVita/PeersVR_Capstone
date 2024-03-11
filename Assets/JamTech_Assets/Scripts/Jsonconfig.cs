835983efcc8087f0ac34d2aec58be6b892910683
JacobMichaud <110305423+JacobMichaud@users.noreply.github.com>
2024-03-11 17:44:04 -0400

    added functionality and dynamic data changing to lesson view, Jacob

--- Assets/JamTech_Assets/Scripts/Jsonconfig.cs
+++ Assets/JamTech_Assets/Scripts/Jsonconfig.cs
@@ -10,38 +10,7 @@ public class Jsonconfig : MonoBehaviour
 
     void Start()
     {
-        // Load lesson data
-        lessonData = LoadLessonData();
-        if (lessonData != null)
-        {
-            // Log Lesson 1 data
-            Debug.Log("Lesson 1 Title: " + lessonData.lesson1.title);
-            Debug.Log("Lesson 1 Description: " + lessonData.lesson1.description);
-            Debug.Log("Lesson 1 Example Videos: ");
-            foreach (string videoUrl in lessonData.lesson1.non_example_videos)
-            {
-                Debug.Log("- " + videoUrl);
-            }
-            Debug.Log("Lesson 1 Exercises: ");
-            foreach (string exercise in lessonData.lesson1.exercises)
-            {
-                Debug.Log("- " + exercise);
-            }
-
-            // Log Lesson 2 data
-            Debug.Log("Lesson 2 Title: " + lessonData.lesson2.title);
-            Debug.Log("Lesson 2 Description: " + lessonData.lesson2.description);
-            Debug.Log("Lesson 2 Example Videos: " + lessonData.lesson2.example_videos);
-            foreach (string videoUrl in lessonData.lesson2.non_example_videos)
-            {
-                Debug.Log("- " + videoUrl);
-            }
-            Debug.Log("Lesson 2 Exercises: ");
-            foreach (string exercise in lessonData.lesson2.exercises)
-            {
-                Debug.Log("- " + exercise);
-            }
-        }
+        
     }
 
     public LessonData LoadLessonData()
--- Assets/JamTech_Assets/Scripts/LessonData.cs
+++ Assets/JamTech_Assets/Scripts/LessonData.cs
@@ -10,9 +10,13 @@ public class LessonData {
 public class Lesson {
     public string title;
     public string[] non_example_videos;
+    public string[] non_example_thumbnails;
     public string description;
     public string more_info;
-    public string example_videos;
+    public string[] non_example_titles;
+    public string[] example_titles;
+    public string[] example_videos;
+    public string[] example_thumbnails;
     public string[] exercises;
     public string quiz_url;
 }
